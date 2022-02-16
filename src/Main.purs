66877b243aecd1ec0dd8bd7700221416947380d5
injuly <srijannr1@gmail.com>
2022-02-17 00:46:29 +0530

    removed unused imports

--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,14 @@
 module Main where
 
-import Data.Maybe
-
 import Data.Either (Either(..))
-import Data.Traversable (for_, traverse)
+import Data.Traversable (for_)
 import Effect (Effect)
 import Effect.Class.Console (error)
 import Effect.Console (log)
-import Foreign.Object as FO
 import Murr.Core (getVulnJSPkgs, parsePackageJson)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
-import Prelude (Unit, discard, show, bind, (<$>), pure, unit, (<<<))
+import Prelude (Unit, discard, bind, (<$>))
 
 main :: Effect Unit
 main = do
@@ -22,5 +19,5 @@ main = do
     packageJson = parsePackageJson contents
     vuln = getVulnJSPkgs <$> packageJson
   case vuln of
-    Left errMsg -> log errMsg
+    Left errMsg -> error errMsg
     Right xs -> for_ xs log
